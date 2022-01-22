4dc4e50a0f2880c46ebdf9e90efa5339f9600c55
UdayJadhav3421 <udaysjadhav3@gmail.com>
2022-01-22 12:49:16 +0530

    Cpp Pragramms

---
+++ Basic Programms/01. Addition of Two Numbers.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int Num1 = 0, Num2 = 0,Sum = 0;
+
+    cout<<"\n Enter 1st Number =";
+    cin>>Num1;
+
+    cout<<"\n Enter 2nd Number =";
+    cin>>Num2;
+
+    Sum = Num1 + Num2;
+
+    cout<<"\n Sum "<<Num1 <<" + "<<Num2 << " is = " <<Sum <<".";
+
+    getch();
+    return 0;
+}
